2447981264345fa6cf0cc57ba71a874d38e42da0
mitra-miri <mitra0.0miri@gmail.com>
2026-05-27 18:40:37 +0330

    js

--- js/quiz/js.js
+++ js/quiz/js.js
@@ -25,14 +25,21 @@ const data = [
 
 ];
 
+
+let score = 0
+let wrongAnswer = []
+let currentQuestion = 0;
+const questionContainerElement = document.getElementById('question-container')
+const subBtnElement = document.getElementById('submit-btn');
 const questionElement = document.getElementById('question');
 const choicesElement = document.getElementById('choices');
 const resultElement = document.getElementById('result');
 
-function q1(){
-    const {question,choices} = data[0]
+function loadQ(){
+    const {question,choices} = data[currentQuestion]
     questionElement.textContent = question;
     
+    choicesElement.innerHTML = ''
     choices.forEach((choice, index) => {
         const choiceBtn = document.createElement('button')
         choicesElement.appendChild(choiceBtn)
@@ -50,4 +57,58 @@ function selectAns(index){
     });
     choicesElement.children[index].classList.add('selectedBtn')
 }
-q1()
+
+function nextQuestion(){
+    const selectedBtn = choicesElement.querySelector('.selectedBtn')
+    if(!selectedBtn) return
+
+    if([...choicesElement.children].indexOf(selectedBtn) === data[currentQuestion].correctAnswer)
+        score +=1;
+    else{
+        indexOfWrongAns =[...choicesElement.children].indexOf(selectedBtn);
+        
+
+        wrongAnswer.push({Question: data[currentQuestion].question , 
+            YourAnswer :data[currentQuestion].choices[indexOfWrongAns],
+            correctAnswer:data[currentQuestion].choices[data[currentQuestion].correctAnswer]
+        })
+    }
+    currentQuestion += 1;
+
+    if (currentQuestion >= data.length) showResult()
+    else loadQ()
+
+
+    
+}
+
+function showResult(){
+    questionContainerElement.style.display = 'none'
+    subBtnElement.style.display = 'none'
+    temp=
+    `
+    <p class ='score'>You scored ${Math.round((score*100)/data.length)} % 
+    ( ${score} out of ${data.length})
+    </p>
+    `
+    if(wrongAnswer.length !=0){
+        temp += '<h6>Wrong answer</h6>'
+        temp += '<ul>'
+        wrongAnswer.forEach ((wrong)=>{
+            temp +=
+                `<li>
+                    <p>Question : ${wrong.Question}</p>
+                    <p>Your answer : <span class='wrong-style'>${wrong.YourAnswer}</span></p>
+                    <p>Correct Answer : <span class='correct-style'>${wrong.correctAnswer}</span></p>
+
+                </li>`
+
+        })
+        temp +='</ul>'
+        resultElement.innerHTML = temp;
+    }
+
+}
+
+subBtnElement.addEventListener('click',nextQuestion)
+loadQ()
